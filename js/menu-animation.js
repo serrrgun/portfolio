e816893a6d9e31e24af0175650ce48f991699b9b
serrrgun <serrrgun@github.com>
2018-03-21 11:43:33 +0300

    добавляет отправку формы

--- js/menu-animation.js
+++ js/menu-animation.js
@@ -1,14 +1,14 @@
 'use strict';
 var toggleButton = document.querySelector('.menu-icon');
 var menuToggle = document.querySelector('.header-slide__menu');
-var menuClosedClick = document.querySelector('.header-slide__menu-item');
+var menuClosedClick = document.querySelector('.header-slide__menu-items');
+
 toggleButton.addEventListener('click', (function(){
   toggleButton.classList.toggle('clicked');
   menuToggle.classList.toggle('block__menu');
 }));
 
 menuClosedClick.addEventListener('click', (function(){
-  
+  toggleButton.classList.toggle('clicked');
   menuToggle.classList.toggle('block__menu');
-  
 }));
